16a3e027a57e944c5c1ff1f69b261acff5ee0bbb
subhasis <55424474+Striker70@users.noreply.github.com>
2022-03-27 21:09:35 +0530

    last commit

---
+++ src/components/card.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Cards = ({ item, handleClick }) => {
+  const { title, author, price, img } = item;
+  return (
+    <>
+      {/*<div className="cards">
+        <div className="image_box">
+          <img src={img} alt="" />
+        </div>
+        <div className="details">
+          <p>{title}</p>
+          <p>{author}</p>
+          <p>Price - {price}Rs</p>
+          <button onClick={() => handleClick(item)}>Add to Cart</button>
+        </div>
+  </div>*/}
+      <div class="cards">
+        <div className="image_box">
+          <img src={img} alt="" />
+        </div>
+        <div className="details">
+          <h5>{title}</h5>
+          <p>{author}</p>
+          <p>Price - {price}Rs</p>
+          <button onClick={() => handleClick(item)}>Add to Cart</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cards;
